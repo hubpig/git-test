8f210ea02d5d8d7f1c28ca593a29a6f1c472682f
changbin <1740136268@qq.com>
2021-10-11 17:36:05 +0800

    second commit

--- src/main/java/zcb/study/GitTest.java
+++ src/main/java/zcb/study/GitTest.java
@@ -3,5 +3,6 @@ package zcb.study;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
